44f2ffc0fc04a8968582320aeeb5a7ba7a5f0db2
Denys <denisburduzan@gmail.com>
2023-12-09 17:31:58 +0200

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let count = 0;
-
-while(count < 1000){
-    if(count === 6 || count === 28 || count === 496)
-    console.log(count);
-    count++;
-}
+function is_perfect(number) {
+    var temp = 0;
+       for(var i=1;i<=number/2;i++) {
+             if(number%i === 0) {
+                temp += i;
+              }
+         }
+      
+         if(temp === number && temp !== 0) {
+           console.log("Это идеальное число.");
+            }  
+          else {
+           console.log("Это не идеальное число.");
+            }  
+     }
+     is_perfect(30);
+    
